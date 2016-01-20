aa3d2743e8f234448529e9874969a35dae24b262
cjw <fk911c@gmail.com>
2016-01-20 17:48:11 +0800

    Initial Commit

---
+++ CJWOC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CJWOC
+//
+//  Created by Frank on 1/20/16.
+//  Copyright © 2016 Frank. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
